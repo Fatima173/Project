9070c553fa49829c4bc78f581a5d0a0308b81c77
fatima <nadeemfatima302@gmail.com>
2023-06-12 14:50:33 +0500

    project

---
+++ src/main/java/com/mycompany/project/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hamza Nadeem
+ */
+public class main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+       
+       database db = new database();
+       db.mycon();
+       
+     
+    }
+}
